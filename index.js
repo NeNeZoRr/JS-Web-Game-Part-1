09f21af1f547aaff9b5d0ed277dfa7dc38afcf48
NeNeZoRr <zorrillalin@gmail.com>
2023-08-30 13:25:12 -0400

    Updated 	modified:   index.js

--- index.js
+++ index.js
@@ -28,7 +28,19 @@ function arc(url, left, bottom){
 })
 }
 //Bonus Back Ground 
+function backDrop (url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++)
+        devArc(url, left + w*96, bottom+h*100)
+    }
+  }
 
+let horizon = window.innerHeight / 2.32
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+backDrop('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+backDrop('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 devArc('assets/green-character.gif', 100, 100)
 devArc('assets/pine-tree.png', 450, 200)
@@ -36,6 +48,7 @@ devArc('assets/tree.png', 200, 300)
 devArc('assets/pillar.png', 350, 100)
 devArc('assets/crate.png' , 150, 200)
 devArc('assets/well.png', 500, 425)
+
 arc('assets/sword.png', 500, 405)
 arc('assets/shield.png', 165, 185)
 arc('assets/staff.png', 600, 100)
